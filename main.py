4234dd765fea8a6a5927bd9949164ad311cb5a89
0xHamz <60563853+0xHamz@users.noreply.github.com>
2023-10-26 23:12:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ plt.xlabel('Nama Barang')
 plt.ylabel('Stok Awal')
 plt.title('Stok Barang')
 plt.xticks(rotation=45)
-plt.show()
+st.pyplot(plt)
 
 
 #pengeluaran
@@ -26,7 +26,7 @@ plt.xlabel('Nama Barang')
 plt.ylabel('Pengeluaran')
 plt.title('Pengeluaran')
 plt.xticks(rotation=45)
-plt.show()
+st.pyplot(plt)
 
 #Pemasukan
 plt.figure(figsize=(8, 6))
@@ -35,7 +35,7 @@ plt.xlabel('Nama Barang')
 plt.ylabel('Pemasukkan')
 plt.title('Pemasukan')
 plt.xticks(rotation=45)
-plt.show()
+st.pyplot(plt)
 
 # Membuat DataFrame laba yang hanya berisi 15 data pertama
 laba_15_data = data_toko[['Tanggal', 'Laba']][:15]
@@ -47,6 +47,55 @@ plt.xlabel('Tanggal')
 plt.ylabel('Laba')
 plt.title('Laba (15 Data Pertama)')
 plt.xticks(rotation=45)
-plt.show()
+st.pyplot(plt)
 
 
+# %%
+import pandas as pd
+import locale
+# Membaca data dari file CSV 
+data_toko = pd.read_csv('data_toko.csv')
+
+
+# Set konfigurasi lokal
+locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
+
+# Fungsi untuk mengubah format angka ke Rupiah
+def format_rupiah(value):
+    return locale.currency(value, grouping=True)
+
+# Menghitung Total Pengeluaran dan Total Pemasukan
+total_pengeluaran = data_toko['Pengeluaran'].sum()
+total_pemasukan = data_toko['Pemasukkan'].sum()
+
+
+stok_barang = data_toko[['Nama Barang', 'Stok Awal', 'Barang Terjual']]
+
+
+laba = data_toko[['Tanggal', 'Pengeluaran', 'Pemasukkan', 'Laba']]
+laba_toko = pd.DataFrame(laba)
+
+
+# Mengubah format kolom Laba ke Rupiah
+laba_toko['Laba'] = data_toko['Laba'].map(format_rupiah)
+laba_toko['Pemasukkan'] = data_toko['Pemasukkan'].map(format_rupiah)
+laba_toko['Pengeluaran'] = data_toko['Pengeluaran'].map(format_rupiah)
+
+# Menghitung Stok Akhir
+stok_akhir = data_toko['Stok Awal'].sum() - data_toko['Barang Terjual'].sum()
+
+# Menampilkan Stok Barang
+print("Stok Barang:")
+print(stok_barang)
+
+# Menampilkan 
+print("\nLaba:")
+print(laba_toko)
+
+
+# Menampilkan Total Pengeluaran, Total Pemasukan, dan Stok Akhir
+print("\nTotal Pengeluaran:", total_pengeluaran)
+print("Total Pemasukan:", total_pemasukan)
+print("Stok Akhir:", stok_akhir)
+
+# %%
